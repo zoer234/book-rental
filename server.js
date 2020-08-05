9c61cd055fcec81c617135a4637c4723b6ea4bb4
zoer234 <spking3@hanmail.net>
2020-08-05 14:54:03 +0900

    add

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
+const books = require("./routes/books");
+const users = require("./routes/users");
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/users", users);
+app.use("/api/v1/books", books);
 
 const PORT = process.env.PORT || 5322;
 
